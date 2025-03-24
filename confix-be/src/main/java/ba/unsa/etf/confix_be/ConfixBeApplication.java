7ecc636486e9a8c83a3de23b5ec2e7755e1587ec
e-Smajic <esmajic2@etf.unsa.ba>
2025-03-24 22:39:37 +0100

    Added spring boot starter project

---
+++ confix-be/src/main/java/ba/unsa/etf/confix_be/ConfixBeApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.confix_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfixBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfixBeApplication.class, args);
+	}
+
+}
